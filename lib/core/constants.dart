f502ab9e712f7aebaa7f372e84eea7649fc3c78d
Eduardo Fadel Picheth <duhpicheth@gmail.com>
2024-09-10 15:24:42 +0200

    create constants file

---
+++ lib/core/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  //Size elements
+  static const double poiCardHeight = 100.0;
+  static const double poiImageHeight = 100.0;
+  static const double mediumSizeSpace = 24.0;
+  static const double smallSizeSpace = 10.0;
+  static const double poiButtonHeight = 40.0;
+
+  //Strings
+  static const String baseUrl = 'https://raw.githubusercontent.com/';
+  static const String pathUrl = 'worldline-spain/technical_test_backend/main/pois.json';
+
+  //Colors
+  static const redColor = Colors.red;
+  static const whiteColor = Colors.white;
+}
