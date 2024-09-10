8ef3eb662a6cc536e172203602b795bf4703aae4
Eduardo Fadel Picheth <duhpicheth@gmail.com>
2024-09-10 17:18:47 +0200

    add equality to avoid problems with diff lists

--- lib/features/home/cubit/home_state.dart
+++ lib/features/home/cubit/home_state.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/foundation.dart';
+
 import 'package:points_of_interests/core/models/point_of_interest_ui.dart';
 
 class HomeState {
@@ -16,4 +18,16 @@ class HomeState {
       errorMessage?? this.errorMessage
     );
   }
+
+  @override
+  bool operator ==(covariant HomeState other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      listEquals(other.pointsOfInterests, pointsOfInterests) &&
+      other.errorMessage == errorMessage;
+  }
+
+  @override
+  int get hashCode => pointsOfInterests.hashCode ^ errorMessage.hashCode;
 }
